0c3059c6f0e15a42c3b582b0a975dcd110f5e1b2
Tim Savage <tim.savage@gmail.com>
2022-01-06 01:50:05 -0600

    update javadoc

--- src/main/java/com/winterhavenmc/util/messagebuilder/YamlFileInstaller.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/YamlFileInstaller.java
@@ -8,9 +8,9 @@ import java.util.*;
 
 /**
  * Install localization files from language directory in jar archive to language subdirectory
- * of the plugin data directory. Any files with a .yml suffix that are stored as a resource
- * within a /language subdirectory in the plugin jar archive will be copied to the /language
- * subdirectory of the plugin data directory.
+ * of the plugin data directory. Any files listed in auto_install.txt file with a .yml suffix
+ * that are stored as a resource within a /language subdirectory in the plugin jar archive will be
+ * copied to the /language subdirectory of the plugin data directory.
  */
 class YamlFileInstaller {
 
