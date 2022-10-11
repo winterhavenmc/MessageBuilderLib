abd262d02bf77258053ea43a21849fe918192c9a
Tim Savage <tim.savage@gmail.com>
2022-10-11 04:24:18 -0500

    split macros into distinct classes, with interface, abstract skeletal class and registry

---
+++ src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/AbstractProcessor.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.util.messagebuilder.macro.processor;
+
+import com.winterhavenmc.util.messagebuilder.LanguageHandler;
+import com.winterhavenmc.util.messagebuilder.macro.MacroObjectMap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public abstract class AbstractProcessor {
+
+	protected final JavaPlugin plugin;
+	protected final LanguageHandler languageHandler;
+	protected final static String UNKNOWN_VALUE = "???";
+
+
+	AbstractProcessor(JavaPlugin plugin, LanguageHandler languageHandler) {
+		this.plugin = plugin;
+		this.languageHandler = languageHandler;
+	}
+
+	public abstract ResultMap doReplacements(final MacroObjectMap macroObjectMap, final String key, final Object object);
+
+
+	protected ResultMap mapName(final String key, final String name) {
+
+		ResultMap resultMap = new ResultMap();
+
+		String keyExtra = "_NAME";
+		if (key.endsWith("_NAME")) {
+			keyExtra = "";
+		}
+
+		resultMap.put(key, name);
+		resultMap.put(key + keyExtra, name);
+
+		return resultMap;
+	}
+
+
+	ResultMap mapConfigItemName(final MacroObjectMap macroObjectMap, final String... keys) {
+
+		ResultMap resultMap = new ResultMap();
+
+		// put item name in result map for each passed key
+		for (String key : keys) {
+			resultMap.put(key, getConfigItemName(macroObjectMap));
+		}
+		return resultMap;
+	}
+
+
+	private String getConfigItemName(final MacroObjectMap macroObjectMap) {
+
+		int itemQuantity = 1;
+
+		// if macro ITEM_QUANTITY exists and is integer, set quantity
+		if (macroObjectMap.containsKey("ITEM_QUANTITY") && macroObjectMap.get("ITEM_QUANTITY") instanceof Integer integer) {
+			itemQuantity = integer;
+		}
+
+		// get singular or plural item name from message file
+		//noinspection SwitchStatementWithTooFewBranches
+		return switch (itemQuantity) {
+			case 1 -> languageHandler.getItemName().orElse(UNKNOWN_VALUE);
+			default -> languageHandler.getItemNamePlural().orElse(UNKNOWN_VALUE);
+		};
+	}
+
+}
---
+++ src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/OfflinePlayerProcessor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.util.messagebuilder.macro.processor;
+
+import com.winterhavenmc.util.messagebuilder.LanguageHandler;
+import com.winterhavenmc.util.messagebuilder.macro.MacroObjectMap;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class OfflinePlayerProcessor extends AbstractProcessor implements Processor {
+
+	public OfflinePlayerProcessor(JavaPlugin plugin, LanguageHandler languageHandler) {
+		super(plugin, languageHandler);
+	}
+
+	@Override
+	public ResultMap doReplacements(MacroObjectMap macroObjectMap, String key, Object object) {
+
+		ResultMap resultMap = new ResultMap();
+
+		if (object instanceof OfflinePlayer offlinePlayer) {
+			resultMap.put(key, offlinePlayer.getName());
+		}
+
+		return resultMap;
+	}
+
+}
