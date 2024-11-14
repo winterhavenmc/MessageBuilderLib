d4c877e8808adb30426bb9fe7c22beaf2644a74d
Tim Savage <tim.savage@gmail.com>
2024-11-14 10:09:10 -0600

    add todo note

--- src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/LocationProcessor.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/LocationProcessor.java
@@ -39,6 +39,9 @@ public class LocationProcessor extends AbstractProcessor implements Processor {
 
 		Location location = null;
 
+		//todo: these checks are likely obsolete, as an object of types other than location should not get here.
+		// it was a feature intended to give some added flexibility with passing locations for macro replacements.
+
 		// if object is Entity, set use entity location
 		if (object instanceof Entity entity) {
 			location = entity.getLocation();
