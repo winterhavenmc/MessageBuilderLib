d1441b555752ea9aac5aea11794d4e0c9fb7b856
Tim Savage <tim.savage@gmail.com>
2024-11-15 00:07:07 -0600

    add javadocs

--- src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/ProcessorRegistry.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/ProcessorRegistry.java
@@ -20,15 +20,29 @@ package com.winterhavenmc.util.messagebuilder.macro.processor;
 import java.util.EnumMap;
 
 
+/**
+ * This class implements a map of unique macro processor instances using ProcessorType Enum members as keys.
+ * As such, it is backed by an EnumMap.
+ */
 public class ProcessorRegistry {
 
 	private final EnumMap<ProcessorType, Processor> macroProcessorMap = new EnumMap<>(ProcessorType.class);
 
 
+	/**
+	 * This method inserts an entry into the map
+	 * @param type the macro processor type, an Enum member, used as the key
+	 * @param macroProcessor the macro processor instance, used as the value
+	 */
 	public void put(final ProcessorType type, final Processor macroProcessor) {
 		this.macroProcessorMap.put(type, macroProcessor);
 	}
 
+	/**
+	 * This method retrieves a macro processor instance from the map by the MacroProcessorType key
+	 * @param macroProcessorType the macro processor type key
+	 * @return The macro processor instance stored in the map that is referenced by the key
+	 */
 	public Processor get(final ProcessorType macroProcessorType) {
 		return macroProcessorMap.get(macroProcessorType);
 	}
