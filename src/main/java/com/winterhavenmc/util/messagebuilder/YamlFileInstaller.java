cd6f83138b4fd60dc34a4ddae53abc532036b6b7
Tim Savage <tim.savage@gmail.com>
2024-12-13 00:49:50 -0600

    refactor class

--- src/main/java/com/winterhavenmc/util/messagebuilder/YamlFileInstaller.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/YamlFileInstaller.java
@@ -17,7 +17,7 @@
 
 package com.winterhavenmc.util.messagebuilder;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.util.*;
@@ -31,10 +31,58 @@ import java.util.*;
  */
 final class YamlFileInstaller {
 
-	private final JavaPlugin plugin;
+	private final Plugin plugin;
+	private final static String subdirectory = "language";
+	private final static String autoInstallFilename = "auto_install.txt";
+	private final static String autoInstallFullPathname = subdirectory + "/" + autoInstallFilename;
 
-	YamlFileInstaller(final JavaPlugin plugin) {
+
+	/**
+	 * Class constructor
+	 * @param plugin reference to plugin main class
+	 */
+	YamlFileInstaller(final Plugin plugin) {
 		this.plugin = plugin;
+	}
+
+
+	/**
+	 * Install resource files from plugin jar to plugin data directory
+	 */
+	void install() {
+		// iterate over list of language files and install from jar if not already present
+		for (String filename : getAutoInstallFilenames()) {
+			install(filename);
+		}
+	}
+
+
+	/**
+	 * Install resource from plugin jar to plugin data directory
+	 * @param filename {@code String} the filename of the resource to be installed
+	 */
+	void install(final String filename) {
+
+		// get full resource path
+		String resourcePath = subdirectory + "/" + filename;
+
+		// check that resource exists in jar archive
+		if (plugin.getResource(resourcePath) != null) {
+
+			// this check prevents a warning message when files are already installed
+			if (!verifyResourceInstalled(filename)) {
+				// save resource to plugin data directory
+				plugin.saveResource(resourcePath, false);
+
+				// log successful install message
+				plugin.getLogger().info("Installed localization file: " + resourcePath);
+			}
+		}
+		else {
+			// log resource not found error
+			plugin.getLogger().warning("resource '" + resourcePath + "' does not exist in jar archive!");
+		}
+
 	}
 
 
@@ -43,27 +91,25 @@ final class YamlFileInstaller {
 	 *
 	 * @return Set of filename strings
 	 */
-	Collection<String> getResourceFilenames() {
-
-		// auto install file path
-		String resourcePath = "language/auto_install.txt";
+	Collection<String> getAutoInstallFilenames() {
 
 		// use linked hash set to preserve order while eliminating duplicates
 		Collection<String> filenames = new LinkedHashSet<>();
 
 		try {
 			// read file names to be installed from text file
-			Scanner scan = new Scanner(Objects.requireNonNull(plugin.getResource(resourcePath)));
+			Scanner scan = new Scanner(Objects.requireNonNull(plugin.getResource(autoInstallFullPathname)));
 			while (scan.hasNextLine()) {
 				String line = scan.nextLine().strip();
+				// exclude comments and lines not ending with .yml
 				if (!line.startsWith("#") && line.endsWith(".yml")) {
 					filenames.add(line);
 				}
 			}
 		}
 		catch (NullPointerException e) {
-			// log resource not found error
-			plugin.getLogger().warning("resource '" + resourcePath + "' does not exist in jar archive!");
+			// log resource not found error if language/auto_install.txt is not present in jar archive
+			plugin.getLogger().warning("resource '" + autoInstallFullPathname + "' does not exist in jar archive!");
 		}
 
 		return filenames;
@@ -71,37 +117,21 @@ final class YamlFileInstaller {
 
 
 	/**
-	 * Install files from plugin jar to plugin data directory
-	 *
-	 * @param fileList collection of filenames to be installed
+	 * Test if language/auto_install.txt file resource is embedded in jar
+	 * @return boolean {@code true} if embedded auto install resource file exists, {@code false} if it does not
 	 */
-	void installResourceFiles(final Collection<String> fileList) {
+	boolean verifyResourceExists(final String filename) {
+		return this.plugin.getResource(filename) != null;
+	}
 
-		// iterate over list of language files and install from jar if not already present
-		for (String filename : fileList) {
 
-			// get full resource path
-			String resourcePath = "language/" + filename;
-
-			// check file exists in jar archive
-			if (plugin.getResource(resourcePath) != null) {
-
-				// this check prevents a warning message when files are already installed
-				if (new File(plugin.getDataFolder() + File.separator + "language" + File.separator + filename).exists()) {
-					continue;
-				}
-
-				// save resource to plugin data directory
-				plugin.saveResource(resourcePath, false);
-
-				// log successful install message
-				plugin.getLogger().info("Installed localization file: " + resourcePath);
-			}
-			else {
-				// log resource not found error
-				plugin.getLogger().warning("resource '" + resourcePath + "' does not exist in jar archive!");
-			}
-		}
+	/**
+	 * Test if resource is installed in language subdirectory within plugin data directory
+ 	 * @param filename the filename to verify
+	 * @return boolean true if a file with the filename exists in the plugin data directory, false if not
+	 */
+	boolean verifyResourceInstalled(final String filename) {
+		return new File(plugin.getDataFolder() + File.separator + subdirectory + File.separator + filename).exists();
 	}
 
 }
