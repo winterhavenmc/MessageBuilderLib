e3c2fc2bf3af3562fb780e1989ee9c7473defd22
Tim Savage <tim.savage@gmail.com>
2024-12-27 04:42:39 -0600

    add record data objects for holding values retrieved from backing stores

---
+++ src/main/java/com/winterhavenmc/util/messagebuilder/MessageRecord.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.util.messagebuilder;
+
+
+public record MessageRecord(
+		String messageKey,
+		boolean enabled,
+		String message,
+		long repeatDelay,
+		String title,
+		int titleFadeIn,
+		int titleStay,
+		int titleFadeOut,
+		String subtitle) { }
