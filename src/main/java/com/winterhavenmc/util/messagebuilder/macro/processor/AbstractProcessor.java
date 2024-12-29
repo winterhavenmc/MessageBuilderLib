b51192f3ac447dade4b85a5d6c70cbd725541004
Tim Savage <tim.savage@gmail.com>
2024-12-29 02:35:48 -0600

    simplify AbstractProcessor class

--- src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/AbstractProcessor.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/macro/processor/AbstractProcessor.java
@@ -17,69 +17,27 @@
 
 package com.winterhavenmc.util.messagebuilder.macro.processor;
 
-import com.winterhavenmc.util.messagebuilder.LanguageHandler;
-import com.winterhavenmc.util.messagebuilder.macro.MacroObjectMap;
+import com.winterhavenmc.util.messagebuilder.query.QueryHandler;
 
 
-public abstract class AbstractProcessor {
+/**
+ * This skeleton class makes the queryHandler available to any extending classes,
+ * as well as the UNKNOWN_VALUE string constant.
+ * Methods used by all classes may be placed here in the future.
+ */
+public abstract class AbstractProcessor implements Processor {
 
-	protected final LanguageHandler languageHandler;
-	protected final static String UNKNOWN_VALUE = "???";
+	final static String UNKNOWN_VALUE = "???";
 
-
-	AbstractProcessor(final LanguageHandler languageHandler) {
-		this.languageHandler = languageHandler;
-	}
-
-	public abstract ResultMap execute(final MacroObjectMap macroObjectMap, final String key, final Object object);
-
-
-	protected ResultMap mapName(final String key, final String name) {
-
-		ResultMap resultMap = new ResultMap();
-
-		//TODO: This creates a key with the original key string, and additionally a second key suffixed with '_NAME' if that is not already the case.
-		// This was done for backward compatibility purposes, and will be removed or modified at some point in the future
-
-		String keyExtra = "_NAME";
-		if (key.endsWith("_NAME")) {
-			keyExtra = "";
-		}
-
-		resultMap.put(key, name);
-		resultMap.put(key + keyExtra, name);
-
-		return resultMap;
-	}
+	public final QueryHandler queryHandler;
 
 
-	ResultMap mapConfigItemName(final MacroObjectMap macroObjectMap, final String... keys) {
-
-		ResultMap resultMap = new ResultMap();
-
-		// put item name in result map for each passed key
-		for (String key : keys) {
-			resultMap.put(key, getConfigItemName(macroObjectMap));
-		}
-		return resultMap;
-	}
-
-
-	private String getConfigItemName(final MacroObjectMap macroObjectMap) {
-
-		int itemQuantity = 1;
-
-		// if macro ITEM_QUANTITY exists and is integer, set quantity
-		if (macroObjectMap.containsKey("ITEM_QUANTITY") && macroObjectMap.get("ITEM_QUANTITY") instanceof Integer integer) {
-			itemQuantity = integer;
-		}
-
-		// get singular or plural item name from message file
-		//noinspection SwitchStatementWithTooFewBranches
-		return switch (itemQuantity) {
-			case 1 -> languageHandler.getItemName().orElse(UNKNOWN_VALUE);
-			default -> languageHandler.getItemNamePlural().orElse(UNKNOWN_VALUE);
-		};
+	/**
+	 * Abstract class constructor
+	 * @param queryHandler the queryHandler used by the classes that extend this abstract class
+	 */
+	AbstractProcessor(final QueryHandler queryHandler) {
+		this.queryHandler = queryHandler;
 	}
 
 }
