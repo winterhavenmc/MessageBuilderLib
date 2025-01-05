cad9f0f1deca7a243390270c4a97b3e7cf6001e4
Tim Savage <tim.savage@gmail.com>
2025-01-04 19:19:42 -0600

    edit javadocs

--- src/main/java/com/winterhavenmc/util/messagebuilder/resolvers/quantity/QuantityResolver.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/resolvers/quantity/QuantityResolver.java
@@ -25,7 +25,7 @@ import java.util.Optional;
  * Resolver for Quantifiable objects with an associated quantity. Any object that has a known method
  * for retrieving an {@code int} quantity will be returned as a Quantifiable object type, with a
  * {@code getQuantity()} method. This method will be mapped to the actual method of the object that returns an
- * {@code int} quantity, regardless of its actual method name. Any object that is not known to have a
+ * {@code int} quantity, regardless of its method name. Any object that is not known to have a
  * quantity will result in an empty {@code Optional} being returned from the static {@code asQuantifiable} method.
  */
 public class QuantityResolver {
