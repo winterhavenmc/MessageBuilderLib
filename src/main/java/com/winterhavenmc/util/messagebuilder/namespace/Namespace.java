cd7fe1c3dc04817b85d2e3e0afe807346a746656
Tim Savage <tim.savage@gmail.com>
2025-01-07 03:44:40 -0600

    add Source enum; add comments

--- src/main/java/com/winterhavenmc/util/messagebuilder/namespace/Namespace.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/namespace/Namespace.java
@@ -29,23 +29,37 @@ public class Namespace {
 	 * This enum represents the top-level domains that must be used as the root of the keyDomain.
 	 * They are subject to change at this time, and will likely be reduced to an appropriate set as necessary.
 	 * I selected a wide array of domains here until I narrow down those I find useful.
+	 * <p>
+	 * NOTE: THIS ENUM REPRESENTS THE SOURCE OF TRUTH FOR DOMAIN VALUES, USED IN PRODUCTION. IT MAY BE MOVED TO
+	 * A MORE APPROPRIATE LOCATION IN THE FUTURE
+	 * </P>
 	 */
 	public enum Domain {
-		ITEMS, // values supplied by the yaml language file, from the root level section 'ITEMS'
 		CONSTANTS, //values supplied by the yaml language file, from the root level section 'CONSTANTS'
+		ITEMS, // values supplied by the yaml language file, from the root level section 'ITEMS'
 		MACRO, // values passed in by calls to the setMacro method
-		//MESSAGES, // probably should be here eventually
-
-		// potential future domains
-//		ENV, // environment variables, such as those made available by the operating system
-//		RUNTIME, // runtime variables, such as those made available by the Java Runtime Environment
-//		SERVER, // server variables, made available by the bukkit server instance running the plugin
-//		PLUGIN, // plugin variables (could use subdomains to hold info about other plugins)
-//		LIBRARY, // library variables, those specific to this library
-//		RESERVED, // not for you variables?
+		MESSAGES, // values supplied by the yaml language file. from the root level section 'MESSAGES'
+		TIME, // string values for time units supplied by the yaml language file, from the root level section 'TIME'
 	}
 
 
+	/**
+	 * NOTE: THIS ENUM IS NOT CURRENTLY IN USE. IT IS A DEMONSTRATION OF POSSIBLE SOURCE CONSTANTS
+	 * THAT WERE CONSIDERED ALONG WITH DOMAIN CONSTANTS. IF IT IS NOT USED IN PRODUCTION, REMOVE IT OR MOVE TO TESTING.
+	 */
+	public enum Source {
+		ENV, // environment variables, such as those made available by the operating system
+		RUNTIME, // runtime variables, such as those made available by the Java Runtime Environment
+		SERVER, // server variables, made available by the bukkit server instance running the plugin
+		PLUGIN, // plugin variables (could use subdomains to hold info about other plugins)
+		LIBRARY, // library variables, those specific to this library
+		RESERVED, // not for you variables?
+	}
+
+
+	/**
+	 * NOTE: THIS ENUM IS CURRENTLY IN USE FOR TESTING. IF IT IS NOT USED FOR PRODUCTION, MOVE IT TO TESTING.
+	 */
 	public static class Time {
 		// this enum contains the default en-US singular and plural names of time units.
 		// It is used only for testing at this time, to confirm the correct singular and
@@ -85,6 +99,8 @@ public class Namespace {
 	// these should reflect the field names as they will be used as suffixes in the context keys
 	// required should mean that the retrieved value may be null. if this meaning is inappropriate
 	// for the field 'required', a new field should be added to positively identify nullable fields.
+	//
+ 	// NOTE: THIS ENUM HAS BEEN SUPERSEDED IN USE BY A SIMILAR ENUM DECLARED WITHIN THE ITEM RECORD CLASS
 	public static class Item {
 		public enum Field {
 			// fields in source
@@ -148,6 +164,9 @@ public class Namespace {
 		}
 	}
 
+	/**
+	 * This enum serves to document the fields that will be put in place for the message recipient in the context map
+	 */
 	public static class Recipient {
 		public enum Field {
 			NAME("RECIPIENT.NAME", String.class, "The name of the recipient"),
