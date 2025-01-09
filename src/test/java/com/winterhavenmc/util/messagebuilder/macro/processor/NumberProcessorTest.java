e35a8cf64e5f1a7223f8935b0ec002cfd2208803
Tim Savage <tim.savage@gmail.com>
2025-01-08 19:55:46 -0600

    use Mockito #Mock

--- src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/NumberProcessorTest.java
+++ src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/NumberProcessorTest.java
@@ -17,106 +17,130 @@
 
 package com.winterhavenmc.util.messagebuilder.macro.processor;
 
-import com.winterhavenmc.util.messagebuilder.query.LanguageFileQueryHandler;
+import com.winterhavenmc.util.messagebuilder.macro.ContextContainer;
+import com.winterhavenmc.util.messagebuilder.macro.ContextMap;
+import com.winterhavenmc.util.messagebuilder.query.LanguageQueryHandler;
+
+import org.bukkit.entity.Player;
+
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 class NumberProcessorTest {
 
-	private LanguageFileQueryHandler mockLanguageFileQueryHandler;
+	@Mock private LanguageQueryHandler queryHandlerMock;
+	@Mock private Player playerMock;
+
 	private MacroProcessor macroProcessor;
+	private ContextMap contextMap;
 
 
 	@BeforeEach
 	public void setUp() {
-		mockLanguageFileQueryHandler = mock(LanguageFileQueryHandler.class, "MockQueryHandler");
-		macroProcessor = new NumberProcessor(mockLanguageFileQueryHandler);
+		macroProcessor = new NumberProcessor(queryHandlerMock);
+		contextMap = new ContextMap(playerMock);
 	}
 
 	@AfterEach
 	public void tearDown() {
-		mockLanguageFileQueryHandler = null;
+		queryHandlerMock = null;
 		macroProcessor = null;
 	}
 
 
-//	@Test
-//	void execute_integer() {
-//		String key = "SOME_INTEGER";
-//		Integer number = 42;
-//
-//		MacroObjectMap macroObjectMap = new MacroObjectMap();
-//		macroObjectMap.put(key, number);
-//
-//		ResultMap resultMap = macroProcessor.execute(key, number, macroObjectMap);
-//		assertTrue(resultMap.containsKey("SOME_INTEGER"));
-//		assertEquals("42", resultMap.get("SOME_INTEGER"));
-//	}
-//
-//	@Test
-//	void execute_null_integer() {
-//		String key = "SOME_NULL_INTEGER";
-//		Integer number = null;
-//
-//		MacroObjectMap macroObjectMap = new MacroObjectMap();
-//		macroObjectMap.put(key, number);
-//
-//		ResultMap resultMap = macroProcessor.execute(key, number, macroObjectMap);
-//		assertFalse(resultMap.containsKey("SOME_NULL_INTEGER"));
-//	}
-//
-//	@Test
-//	void execute_long() {
-//		String key = "SOME_LONG";
-//		Long number = 420L;
-//
-//		MacroObjectMap macroObjectMap = new MacroObjectMap();
-//		macroObjectMap.put(key, number);
-//
-//		ResultMap resultMap = macroProcessor.execute(key, number, macroObjectMap);
-//		assertTrue(resultMap.containsKey("SOME_LONG"));
-//		assertEquals("420", resultMap.get("SOME_LONG"));
-//	}
-//
-//	@Test
-//	void execute_null_long() {
-//		String key = "SOME_NULL_LONG";
-//		Long number = null;
-//
-//		MacroObjectMap macroObjectMap = new MacroObjectMap();
-//		macroObjectMap.put(key, number);
-//
-//		ResultMap resultMap = macroProcessor.execute(key, number, macroObjectMap);
-//		assertFalse(resultMap.containsKey("SOME_NULL_LONG"));
-//	}
-//
-//	@Test
-//	void execute_duration() {
-//		String key = "SOME_DURATION";
-//		Long number = 12000L;
-//
-//		MacroObjectMap macroObjectMap = new MacroObjectMap();
-//		macroObjectMap.put(key, number);
-//
-//		ResultMap resultMap = macroProcessor.execute(key, number, macroObjectMap);
-//		assertTrue(resultMap.containsKey("SOME_DURATION"));
-//		assertEquals("12 seconds", resultMap.get("SOME_DURATION"));
-//	}
-//
-//	@Test
-//	void execute_duration_minutes() {
-//		String key = "SOME_DURATION_MINUTES";
-//		Long number = 61_000L;
-//
-//		MacroObjectMap macroObjectMap = new MacroObjectMap();
-//		macroObjectMap.put(key, number);
-//
-//		ResultMap resultMap = macroProcessor.execute(key, number, macroObjectMap);
-//		assertTrue(resultMap.containsKey("SOME_DURATION_MINUTES"));
-//		assertEquals("1 minute", resultMap.get("SOME_DURATION_MINUTES"));
-//	}
+	@Test
+	void execute_integer() {
+		// Arrange
+		String key = "SOME_INTEGER";
+		Integer number = 42;
+		contextMap.put(key, ContextContainer.of(number, ProcessorType.NUMBER));
+
+		// Act
+		ResultMap resultMap = macroProcessor.resolveContext(key, contextMap, number);
+
+		// Assert
+		assertTrue(resultMap.containsKey("SOME_INTEGER"));
+		assertEquals("42", resultMap.get("SOME_INTEGER"));
+	}
+
+
+	@Test
+	void execute_null_integer() {
+		// Arrange
+		String key = "SOME_NULL_INTEGER";
+		Integer number = null;
+		contextMap.put(key, ContextContainer.of(number, ProcessorType.NUMBER));
+
+		// Act
+		ResultMap resultMap = macroProcessor.resolveContext(key, contextMap, number);
+
+		// Assert
+		assertFalse(resultMap.containsKey("SOME_NULL_INTEGER"));
+	}
+
+	@Test
+	void execute_long() {
+		// Arrange
+		String key = "SOME_LONG";
+		Long number = 420L;
+		contextMap.put(key, ContextContainer.of(number, ProcessorType.NUMBER));
+
+		// Act
+		ResultMap resultMap = macroProcessor.resolveContext(key, contextMap, number);
+
+		// Assert
+		assertTrue(resultMap.containsKey("SOME_LONG"));
+		assertEquals("420", resultMap.get("SOME_LONG"));
+	}
+
+	@Test
+	void execute_null_long() {
+		// Arrange
+		String key = "SOME_NULL_LONG";
+		Long number = null;
+		contextMap.put(key, ContextContainer.of(number, ProcessorType.NUMBER));
+
+		// Act
+		ResultMap resultMap = macroProcessor.resolveContext(key, contextMap, number);
+
+		// Assert
+		assertFalse(resultMap.containsKey("SOME_NULL_LONG"));
+	}
+
+	@Test
+	void execute_duration() {
+		// Arrange
+		String key = "SOME_DURATION";
+		Long number = 12000L;
+		contextMap.put(key, ContextContainer.of(number, ProcessorType.NUMBER));
+
+		// Act
+		ResultMap resultMap = macroProcessor.resolveContext(key, contextMap, number);
+
+		// Assert
+		assertTrue(resultMap.containsKey("SOME_DURATION"));
+		assertEquals("12 seconds", resultMap.get("SOME_DURATION"));
+	}
+
+	@Test
+	void execute_duration_minutes() {
+		// Arrange
+		String key = "SOME_DURATION_MINUTES";
+		Long number = 61_000L;
+		contextMap.put(key, ContextContainer.of(number, ProcessorType.NUMBER));
+
+		// Act
+		ResultMap resultMap = macroProcessor.resolveContext(key, contextMap, number);
+
+		// Assert
+		assertTrue(resultMap.containsKey("SOME_DURATION_MINUTES"));
+		assertEquals("1 minute", resultMap.get("SOME_DURATION_MINUTES"));
+	}
 
 }
