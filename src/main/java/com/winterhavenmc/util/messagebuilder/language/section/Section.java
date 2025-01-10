e3a709a82b414c7ad6ebc7d27923b4362821e486
Tim Savage <tim.savage@gmail.com>
2025-01-09 21:18:05 -0600

    use bound type for field, parameter and return type that matches any SectionQueryHandler
    Type declaration:
    Class<? extends SectionQueryHandler<?>

--- src/main/java/com/winterhavenmc/util/messagebuilder/language/section/Section.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/language/section/Section.java
@@ -17,10 +17,10 @@
 
 package com.winterhavenmc.util.messagebuilder.language.section;
 
-import com.winterhavenmc.util.messagebuilder.language.section.query.constant.ConstantQueryHandler;
-import com.winterhavenmc.util.messagebuilder.language.section.query.item.ItemQueryHandler;
-import com.winterhavenmc.util.messagebuilder.language.section.query.message.MessageQueryHandler;
-import com.winterhavenmc.util.messagebuilder.language.section.query.time.TimeQueryHandler;
+import com.winterhavenmc.util.messagebuilder.language.section.constants.ConstantQueryHandler;
+import com.winterhavenmc.util.messagebuilder.language.section.item.ItemQueryHandler;
+import com.winterhavenmc.util.messagebuilder.language.section.message.MessageQueryHandler;
+import com.winterhavenmc.util.messagebuilder.language.section.time.TimeQueryHandler;
 
 public enum Section {
 	CONSTANTS(ConstantQueryHandler.class),
@@ -29,14 +29,15 @@ public enum Section {
 	TIME(TimeQueryHandler.class),
 	;
 
-	private final Class<?> handlerType;
+	private final Class<? extends SectionQueryHandler<?>> handlerClass;
 
-	Section(final Class<?> handlerType) {
-		this.handlerType = handlerType;
+
+	Section(final Class<? extends SectionQueryHandler<?>> handlerClass) {
+		this.handlerClass = handlerClass;
 	}
 
-	public Class<?> getHandlerType() {
-		return handlerType;
+	public Class<? extends SectionQueryHandler<?>> getHandlerClass() {
+		return handlerClass;
 	}
 
 }
