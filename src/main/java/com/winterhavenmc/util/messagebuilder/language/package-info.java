b989d0be96bf820c99639ca5b9bb079fd43f9c25
Tim Savage <tim.savage@gmail.com>
2025-01-13 01:28:24 -0600

    edit javadocs

--- src/main/java/com/winterhavenmc/util/messagebuilder/language/package-info.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/language/package-info.java
@@ -18,7 +18,7 @@
 /**
  * This package encapsulates the handling and querying of a language resource. The Interfaces
  * {@code LanguageResourceHandler} and {@code LanguageQueryHandler} declare the methods necessary
- * for an implementation of language resource and query handlers.
+ * for an implementation of language resource and query handlers, respectively.
  * At this time, there is only one implementation of a language resource package, the yaml package.
  */
 package com.winterhavenmc.util.messagebuilder.language;
