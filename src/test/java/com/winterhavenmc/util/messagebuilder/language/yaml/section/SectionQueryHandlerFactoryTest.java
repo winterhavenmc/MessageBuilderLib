215990e18a0a2affbff03f2e6aebdd27ede7aedb
Tim Savage <tim.savage@gmail.com>
2025-01-13 07:48:23 -0600

    initial commit

---
+++ src/test/java/com/winterhavenmc/util/messagebuilder/language/yaml/section/SectionQueryHandlerFactoryTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2025 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.util.messagebuilder.language.yaml.section;
+
+import com.winterhavenmc.util.messagebuilder.language.yaml.YamlConfigurationSupplier;
+import com.winterhavenmc.util.messagebuilder.language.yaml.section.items.ItemSectionQueryHandler;
+import org.bukkit.configuration.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.winterhavenmc.util.messagebuilder.util.MockUtility.loadConfigurationFromResource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SectionQueryHandlerFactoryTest {
+
+	SectionQueryHandlerFactory queryHandlerFactory;
+
+	@BeforeEach
+	void setUp() {
+		Configuration configuration = loadConfigurationFromResource("language/en-US.yml");
+		YamlConfigurationSupplier configurationSupplier = new YamlConfigurationSupplier(configuration);
+
+		queryHandlerFactory = new SectionQueryHandlerFactory(configurationSupplier);
+	}
+
+	@AfterEach
+	void tearDown() {
+		queryHandlerFactory = null;
+	}
+
+	@Test
+	void testConstructor_parameter_null() {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> new SectionQueryHandlerFactory(null));
+
+		assertEquals("The configuration parameter was null.", exception.getMessage());
+	}
+
+
+	@Test
+	void getQueryHandler() {
+		ItemSectionQueryHandler queryHandler = (ItemSectionQueryHandler) queryHandlerFactory.getQueryHandler(Section.ITEMS);
+		assertNotNull(queryHandler);
+	}
+
+	@Test
+	void createSectionHandler() {
+
+	}
+
+	@Test
+	void createSectionHandlerDynamically() {
+	}
+}
