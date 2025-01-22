7d3dc4e5062788b9970ed3556c2d6682b01d254b
Tim Savage <tim.savage@gmail.com>
2025-01-22 00:04:00 -0600

    add test for static create method

--- src/test/java/com/winterhavenmc/util/messagebuilder/context/SourceKeyTest.java
+++ src/test/java/com/winterhavenmc/util/messagebuilder/context/SourceKeyTest.java
@@ -25,84 +25,91 @@ class SourceKeyTest {
 
 	@Test
 	void testConstructor() {
-		SourceKey sourceKey = new SourceKey(Source.CONST, "path/to/constant");
+		SourceKey sourceKey = new SourceKey(Source.CONST, "path.to.constant");
 		assertNotNull(sourceKey);
 		assertEquals(Source.CONST, sourceKey.getSource());
-		assertEquals("path/to/constant", sourceKey.getKeyPath());
+		assertEquals("path.to.constant", sourceKey.getKeyPath());
 	}
 
 	@Test
 	void testGetKey() {
-		SourceKey sourceKey = new SourceKey(Source.ITEM, "item/path");
-		String expectedKey = "ITEM|item/path";
+		SourceKey sourceKey = new SourceKey(Source.ITEM, "item.path");
+		String expectedKey = "ITEM:item.path";
 		assertEquals(expectedKey, sourceKey.getKey());
 	}
 
 	@Test
 	void testToString() {
-		SourceKey sourceKey = new SourceKey(Source.MACRO, "macro/definition");
-		String expectedToString = "MACRO|macro/definition";
+		SourceKey sourceKey = new SourceKey(Source.MACRO, "macro.definition");
+		String expectedToString = "MACRO:macro.definition";
 		assertEquals(expectedToString, sourceKey.toString());
 	}
 
 	@Test
 	void testEqualsSameObject() {
-		SourceKey sourceKey = new SourceKey(Source.CONST, "constant/path");
+		SourceKey sourceKey = new SourceKey(Source.CONST, "constant.path");
 		assertEquals(sourceKey, sourceKey);
 	}
 
 	@Test
 	void testEqualsEqualObjects() {
-		SourceKey key1 = new SourceKey(Source.CONST, "constant/path");
-		SourceKey key2 = new SourceKey(Source.CONST, "constant/path");
+		SourceKey key1 = new SourceKey(Source.CONST, "constant.path");
+		SourceKey key2 = new SourceKey(Source.CONST, "constant.path");
 		assertEquals(key1, key2);
 	}
 
 	@Test
 	void testEqualsDifferentSource() {
-		SourceKey key1 = new SourceKey(Source.CONST, "common/path");
-		SourceKey key2 = new SourceKey(Source.ITEM, "common/path");
+		SourceKey key1 = new SourceKey(Source.CONST, "common.path");
+		SourceKey key2 = new SourceKey(Source.ITEM, "common.path");
 		assertNotEquals(key1, key2);
 	}
 
 	@Test
 	void testEqualsDifferentKeyPath() {
-		SourceKey key1 = new SourceKey(Source.MACRO, "macro/path");
-		SourceKey key2 = new SourceKey(Source.MACRO, "different/path");
+		SourceKey key1 = new SourceKey(Source.MACRO, "macro.path");
+		SourceKey key2 = new SourceKey(Source.MACRO, "different.path");
 		assertNotEquals(key1, key2);
 	}
 
 	@Test
 	void testEqualsNullObject() {
-		SourceKey sourceKey = new SourceKey(Source.ITEM, "item/path");
+		SourceKey sourceKey = new SourceKey(Source.ITEM, "item.path");
 		assertNotEquals(sourceKey, null);
 	}
 
 	@Test
 	void testEqualsDifferentType() {
-		SourceKey sourceKey = new SourceKey(Source.CONST, "constant/path");
+		SourceKey sourceKey = new SourceKey(Source.CONST, "constant.path");
 		String differentType = "Not a SourceKey";
 		assertNotEquals(sourceKey, differentType);
 	}
 
 	@Test
 	void testHashCodeEqualObjects() {
-		SourceKey key1 = new SourceKey(Source.CONST, "constant/path");
-		SourceKey key2 = new SourceKey(Source.CONST, "constant/path");
+		SourceKey key1 = new SourceKey(Source.CONST, "constant.path");
+		SourceKey key2 = new SourceKey(Source.CONST, "constant.path");
 		assertEquals(key1.hashCode(), key2.hashCode());
 	}
 
 	@Test
 	void testHashCodeDifferentSource() {
-		SourceKey key1 = new SourceKey(Source.ITEM, "common/path");
-		SourceKey key2 = new SourceKey(Source.MACRO, "common/path");
+		SourceKey key1 = new SourceKey(Source.ITEM, "common.path");
+		SourceKey key2 = new SourceKey(Source.MACRO, "common.path");
 		assertNotEquals(key1.hashCode(), key2.hashCode());
 	}
 
 	@Test
 	void testHashCodeDifferentKeyPath() {
-		SourceKey key1 = new SourceKey(Source.CONST, "constant/path");
-		SourceKey key2 = new SourceKey(Source.CONST, "different/path");
+		SourceKey key1 = new SourceKey(Source.CONST, "constant.path");
+		SourceKey key2 = new SourceKey(Source.CONST, "different.path");
 		assertNotEquals(key1.hashCode(), key2.hashCode());
 	}
+
+	@Test
+	void testStaticCreate() {
+		String sourceKey = SourceKey.create(Source.MACRO, "some.key.path");
+		assertEquals("MACRO:some.key.path", sourceKey);
+	}
+
 }
