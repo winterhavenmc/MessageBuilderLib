de45911449a5f2cac77eca3c3258ad78aa6990ab
Tim Savage <tim.savage@gmail.com>
2025-01-24 15:41:16 -0600

    remove unused import

--- src/test/java/com/winterhavenmc/util/messagebuilder/MessageTest.java
+++ src/test/java/com/winterhavenmc/util/messagebuilder/MessageTest.java
@@ -31,7 +31,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
