86d03560db8591b9b8b4faf130829b27747346f6
Tim Savage <tim.savage@gmail.com>
2025-01-25 10:54:51 -0600

    add tests

--- src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/LocationProcessorTest.java
+++ src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/LocationProcessorTest.java
@@ -31,8 +31,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -117,6 +118,30 @@ class LocationProcessorTest {
 	}
 
 	@Test
+	void testResolveContext_ValidLocation2() {
+		// Arrange
+		when(worldMock.getName()).thenReturn("test_world");
+		when(locationMock.getWorld()).thenReturn(worldMock);
+		when(locationMock.getBlockX()).thenReturn(123);
+		when(locationMock.getBlockY()).thenReturn(64);
+		when(locationMock.getBlockZ()).thenReturn(-789);
+
+		String key = "PLAYER.LOCATION";
+		contextMap.put(key, locationMock);
+
+		// Act
+		ResultMap result = locationProcessor.resolveContext(key, contextMap);
+
+		// Assert
+		assertThat(result, is(notNullValue()));
+		assertThat(result.get("PLAYER.LOCATION.WORLD"), is("test_world"));
+		assertThat(result.get("PLAYER.LOCATION.X"), is("123"));
+		assertThat(result.get("PLAYER.LOCATION.Y"), is("64"));
+		assertThat(result.get("PLAYER.LOCATION.Z"), is("-789"));
+		assertThat(result.get("PLAYER.LOCATION"), is("test_world [123, 64, -789]"));
+	}
+
+	@Test
 	void testResolveContext_MissingWorld() {
 		// Arrange
 		when(locationMock.getWorld()).thenReturn(null);
@@ -163,6 +188,22 @@ class LocationProcessorTest {
 		assertThat(result.get("HOME.LOCATION.Y"), is("64"));
 		assertThat(result.get("HOME.LOCATION.Z"), is("-789"));
 		assertThat(result.get("HOME.LOCATION"), is("test_world [123, 64, -789]"));
+	}
+
+	@Test
+	void resolveContext_mismatched_type() {
+		// Arrange
+		String keyPath = "SOME_NAME";
+		Duration duration  = Duration.ofMillis(2000);
+		ContextMap contextMap = new ContextMap(playerMock);
+		contextMap.put(keyPath, duration);
+		MacroProcessor macroProcessor = new LocationProcessor();
+
+		// Act
+		ResultMap resultMap = macroProcessor.resolveContext(keyPath, contextMap);
+
+		// Assert
+		assertFalse(resultMap.containsKey(keyPath));
 	}
 
 }
