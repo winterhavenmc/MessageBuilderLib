80eee55a0dc3e63e01429418a098a0c98595675a
Tim Savage <tim.savage@gmail.com>
2025-01-25 01:21:59 -0600

    remove unused import

--- src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/NullProcessorTest.java
+++ src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/NullProcessorTest.java
@@ -30,7 +30,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @ExtendWith(MockitoExtension.class)
