ad9086022464f619e02a8c0032f0d8f4deb10407
Tim Savage <tim.savage@gmail.com>
2025-01-25 08:37:58 -0600

    add tests

--- src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/StringProcessorTest.java
+++ src/test/java/com/winterhavenmc/util/messagebuilder/macro/processor/StringProcessorTest.java
@@ -19,12 +19,15 @@ package com.winterhavenmc.util.messagebuilder.macro.processor;
 
 import com.winterhavenmc.util.messagebuilder.context.*;
 
+import com.winterhavenmc.util.messagebuilder.util.LocalizedException;
 import org.bukkit.entity.Player;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,6 +42,39 @@ class StringProcessorTest {
 	public void tearDown() {
 		playerMock = null;
 	}
+
+
+	@Test
+	void testResolveContext_parameter_null_key() {
+		ContextMap contextMap = new ContextMap(playerMock);
+		MacroProcessor macroProcessor = new StringProcessor();
+		LocalizedException exception = assertThrows(LocalizedException.class,
+				() -> macroProcessor.resolveContext(null, contextMap));
+
+		assertEquals("The parameter 'key' cannot be null.", exception.getMessage());
+	}
+
+
+	@Test
+	void testResolveContext_parameter_empty_key() {
+		ContextMap contextMap = new ContextMap(playerMock);
+		MacroProcessor macroProcessor = new StringProcessor();
+		LocalizedException exception = assertThrows(LocalizedException.class,
+				() -> macroProcessor.resolveContext("", contextMap));
+
+		assertEquals("The parameter 'key' cannot be empty.", exception.getMessage());
+	}
+
+
+	@Test
+	void testResolveContext_parameter_null_context_map() {
+		MacroProcessor macroProcessor = new StringProcessor();
+		LocalizedException exception = assertThrows(LocalizedException.class,
+				() -> macroProcessor.resolveContext("KEY", null));
+
+		assertEquals("The parameter 'contextMap' cannot be null.", exception.getMessage());
+	}
+
 
 	@Test
 	void resolveContext() {
@@ -56,6 +92,23 @@ class StringProcessorTest {
 
 		assertTrue(resultMap.containsKey(keyPath));
 		assertEquals(stringObject, resultMap.get(keyPath));
+	}
+
+	@Test
+	void resolveContext_not_string() {
+
+		String keyPath = "SOME_NAME";
+		Duration duration  = Duration.ofMillis(2000);
+
+		ContextMap contextMap = new ContextMap(playerMock);
+
+		contextMap.put(keyPath, duration);
+
+		MacroProcessor macroProcessor = new StringProcessor();
+
+		ResultMap resultMap = macroProcessor.resolveContext(keyPath, contextMap);
+
+		assertFalse(resultMap.containsKey(keyPath));
 	}
 
 }
