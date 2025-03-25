cdf8b7cd005bf0f10c8ea91cf20278b9786f0c55
Tim Savage <tim.savage@gmail.com>
2025-03-24 19:34:51 -0500

    extract methods to new functional interface classes

--- src/main/java/com/winterhavenmc/util/messagebuilder/pipeline/MacroReplacer.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/pipeline/MacroReplacer.java
@@ -20,17 +20,12 @@ package com.winterhavenmc.util.messagebuilder.pipeline;
 import com.winterhavenmc.util.messagebuilder.Message;
 import com.winterhavenmc.util.messagebuilder.pipeline.processors.*;
 import com.winterhavenmc.util.messagebuilder.resources.language.yaml.section.MessageRecord;
-
 import com.winterhavenmc.util.messagebuilder.validation.ValidationException;
-import org.bukkit.command.CommandSender;
+
 import org.bukkit.entity.Entity;
 
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 import static com.winterhavenmc.util.messagebuilder.validation.MessageKey.PARAMETER_EMPTY;
 import static com.winterhavenmc.util.messagebuilder.validation.MessageKey.PARAMETER_NULL;
@@ -41,23 +36,8 @@ import static com.winterhavenmc.util.messagebuilder.validation.Validator.validat
 /**
  * This class provides handling of the Macro Processors and their Registry
  */
-public class MacroReplacer implements Replacer, Resolver
+public class MacroReplacer implements Replacer
 {
-	private final static String DELIMITER_OPEN = "{";
-	private final static String DELIMITER_CLOSE = "}";
-
-	private final ProcessorRegistry processorRegistry;
-
-
-	/**
-	 * Class constructor
-	 */
-	public MacroReplacer()
-	{
-		this.processorRegistry = new ProcessorRegistry(new DependencyContext());
-	}
-
-
 	/**
 	 * Replace macros in a message to be sent
 	 *
@@ -72,35 +52,10 @@ public class MacroReplacer implements Replacer, Resolver
 
 		// return new message record with final string fields added with macro replacements performed
 		return messageRecord.withFinalStrings(
-				replaceMacros(message.getContextMap(), messageRecord.message()),
-				replaceMacros(message.getContextMap(), messageRecord.title()),
-				replaceMacros(message.getContextMap(), messageRecord.subtitle())
+				replaceMacrosInString(message.getContextMap(), messageRecord.message()),
+				replaceMacrosInString(message.getContextMap(), messageRecord.title()),
+				replaceMacrosInString(message.getContextMap(), messageRecord.subtitle())
 		);
-	}
-
-
-	/**
-	 * Check if a message string contains any macro placeholders by matching delimiters
-	 * containing a valid placeholder string
-	 *
-	 * @param message the message to check for macro placeholders
-	 * @return {@code true} if the message string contains any macro placeholders
-	 */
-	public boolean containsMacros(String message)
-	{
-		return getMatcher(message).find();
-	}
-
-
-	/**
-	 * Create a {@link Stream} of placeholder strings in a message string
-	 *
-	 * @param input the message string with placeholders
-	 * @return a {@code Stream} of placeholder strings
-	 */
-	public Stream<String> getPlaceholderStream(final String input)
-	{
-		return getMatcher(input).results().map(matchResult -> matchResult.group(1));
 	}
 
 
@@ -111,30 +66,15 @@ public class MacroReplacer implements Replacer, Resolver
 	 * @param messageString the message with placeholders to be replaced by macro values
 	 * @return the string with all macro replacements performed
 	 */
-	String replaceMacros(final ContextMap contextMap, final String messageString)
+	String replaceMacrosInString(final ContextMap contextMap, final String messageString)
 	{
 		validate(contextMap, Objects::isNull, () -> new ValidationException(PARAMETER_NULL, CONTEXT_MAP));
 		validate(messageString, Objects::isNull, () -> new ValidationException(PARAMETER_NULL, MESSAGE_STRING));
 		validate(messageString, String::isBlank, () -> new ValidationException(PARAMETER_EMPTY, MESSAGE_STRING));
 
-		// copy message string to local variable
-		String modifiedMessageString = messageString;
-
-		// only process macro tokens if message string contains a pair of macro delimiters
-		if (containsMacros(modifiedMessageString))
-		{
-			// add recipient fields to context map
-			addRecipientContext(contextMap);
-
-			// final result map of keys and processed string values
-			ResultMap replacementStringMap = new ResultMap();
-			replacementStringMap.putAll(resolveContext(contextMap));
-
-			// do macro replacements on message string
-			modifiedMessageString = performReplacements(replacementStringMap, modifiedMessageString);
-		}
-
-		return modifiedMessageString;
+		return Optional.of(messageString)
+				.map(msg -> performReplacements(new ContextResolver().resolve(addRecipientContext(contextMap)), msg))
+				.orElse(messageString);
 	}
 
 
@@ -143,48 +83,19 @@ public class MacroReplacer implements Replacer, Resolver
 	 *
 	 * @param contextMap a map containing key/value pairs of placeholder names and the value objects from which
 	 *                   their replacement strings will be derived
+	 * @return the updated context map, to allow use in functional pipelines
 	 */
-	void addRecipientContext(ContextMap contextMap)
+	ContextMap addRecipientContext(final ContextMap contextMap)
 	{
 		validate(contextMap, Objects::isNull, () -> new ValidationException(PARAMETER_NULL, CONTEXT_MAP));
 
-		// get recipient from context map
-		CommandSender recipient = contextMap.getRecipient();
+		contextMap.getOptionalRecipient().ifPresent(r -> contextMap.put("RECIPIENT", r.getName()));
+		contextMap.getOptionalRecipient()
+				.filter(Entity.class::isInstance)
+				.map(Entity.class::cast)
+				.ifPresent(entity -> contextMap.put("RECIPIENT.LOCATION", entity.getLocation()));
 
-		// put recipient name in context map
-		String key = "RECIPIENT";
-		contextMap.put(key, recipient.getName());
-
-		// if recipient is an entity, put recipient location in macro object map
-		if (recipient instanceof Entity entity)
-		{
-			String locationKey = key.concat(".LOCATION");
-			contextMap.put(locationKey, entity.getLocation());
-		}
-	}
-
-
-	/**
-	 * Convert the value objects contained in the context map into their string representations in a
-	 * new result map.
-	 *
-	 * @param contextMap a map containing key/value pairs of placeholder strings and their corresponding value object
-	 * @return {@code ResultMap} a map containing the placeholder strings and the string representations of the values
-	 */
-	@Override
-	public ResultMap resolveContext(ContextMap contextMap)
-	{
-		validate(contextMap, Objects::isNull, () -> new ValidationException(PARAMETER_NULL, CONTEXT_MAP));
-
-		ResultMap resultMap = new ResultMap();
-		for (Map.Entry<String, Object> entry : contextMap.entrySet())
-		{
-			ProcessorType processorType = ProcessorType.matchType(entry.getValue());
-			MacroProcessor macroProcessor = processorRegistry.get(processorType);
-			resultMap.putAll(macroProcessor.resolveContext(entry.getKey(), contextMap));
-		}
-
-		return resultMap;
+		return contextMap;
 	}
 
 
@@ -201,28 +112,11 @@ public class MacroReplacer implements Replacer, Resolver
 		validate(messageString, Objects::isNull, () -> new ValidationException(PARAMETER_NULL, MESSAGE_STRING));
 		validate(messageString, String::isBlank, () -> new ValidationException(PARAMETER_EMPTY, MESSAGE_STRING));
 
-		String modifiedMessageString = messageString;
-
-		for (Map.Entry<String, String> entry : replacementMap.entrySet())
-		{
-			String macroToken = DELIMITER_OPEN + entry.getKey() + DELIMITER_CLOSE;
-			modifiedMessageString = modifiedMessageString.replace(macroToken, entry.getValue());
-		}
-
-		return modifiedMessageString;
-	}
-
-
-	/**
-	 * Static method to provide a compiled regex pattern matcher, matching message placeholders with delimiters
-	 *
-	 * @return a compiled regex pattern matcher
-	 */
-	static Matcher getMatcher(final String input)
-	{
-		String regex = "\\" + DELIMITER_OPEN + "([\\p{Lu}0-9_]+)" + DELIMITER_CLOSE;
-		Pattern pattern = Pattern.compile(regex);
-		return pattern.matcher(input);
+		return new PlaceholderExtractor().extract(messageString)
+				.reduce(messageString, (msg, placeholder) ->
+						msg.replace( Delimiter.OPEN + placeholder + Delimiter.CLOSE,
+								replacementMap.getValueOrKey(placeholder))
+				);
 	}
 
 }
