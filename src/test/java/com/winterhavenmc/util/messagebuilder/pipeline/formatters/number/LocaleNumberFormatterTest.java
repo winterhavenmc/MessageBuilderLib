22762f021b7625a88da107252d153d4222461bb6
Tim Savage <tim.savage@gmail.com>
2025-04-24 05:23:11 -0500

    add test

---
+++ src/test/java/com/winterhavenmc/util/messagebuilder/pipeline/formatters/number/LocaleNumberFormatterTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2025 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.util.messagebuilder.pipeline.formatters.number;
+
+import com.winterhavenmc.util.messagebuilder.model.locale.LocaleSupplier;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class LocaleNumberFormatterTest
+{
+	@Mock LocaleSupplier localeSupplierMock;
+
+
+	@Test
+	void getFormatted_US()
+	{
+		when(localeSupplierMock.get()).thenReturn(Locale.US);
+		LocaleNumberFormatter formatter = new LocaleNumberFormatter(localeSupplierMock);
+
+		String result = formatter.getFormatted(42000);
+
+		assertEquals("42,000", result);
+	}
+
+
+	@Test
+	void getFormatted_DE()
+	{
+		when(localeSupplierMock.get()).thenReturn(Locale.GERMAN);
+		LocaleNumberFormatter formatter = new LocaleNumberFormatter(localeSupplierMock);
+
+		String result = formatter.getFormatted(42000);
+
+		assertEquals("42.000", result);
+	}
+
+}
