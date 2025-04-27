58c90b6d791a8f4609976ea05d822804bc3b68c8
Tim Savage <tim.savage@gmail.com>
2025-04-27 02:46:16 -0500

    add parameter validation

--- src/main/java/com/winterhavenmc/util/messagebuilder/query/QueryHandlerFactory.java
+++ src/main/java/com/winterhavenmc/util/messagebuilder/query/QueryHandlerFactory.java
@@ -27,6 +27,8 @@ import java.util.Objects;
 
 import static com.winterhavenmc.util.messagebuilder.validation.ErrorMessageKey.PARAMETER_NULL;
 import static com.winterhavenmc.util.messagebuilder.validation.Parameter.LANGUAGE_RESOURCE_MANAGER;
+import static com.winterhavenmc.util.messagebuilder.validation.Parameter.SECTION;
+import static com.winterhavenmc.util.messagebuilder.validation.ValidationHandler.throwing;
 import static com.winterhavenmc.util.messagebuilder.validation.Validator.validate;
 
 
@@ -41,7 +43,7 @@ public class QueryHandlerFactory
 	 */
 	public QueryHandlerFactory(final SectionResourceManager languageResourceManager)
 	{
-		validate(languageResourceManager, Objects::isNull, ValidationHandler.throwing(PARAMETER_NULL, LANGUAGE_RESOURCE_MANAGER));
+		validate(languageResourceManager, Objects::isNull, throwing(PARAMETER_NULL, LANGUAGE_RESOURCE_MANAGER));
 
 		this.languageResourceManager = languageResourceManager;
 	}
@@ -55,6 +57,8 @@ public class QueryHandlerFactory
 	 */
 	public <R extends SectionRecord> QueryHandler<R> getQueryHandler(final Section section)
 	{
+		validate(section, Objects::isNull, throwing(PARAMETER_NULL, SECTION));
+
 		SectionProvider provider = languageResourceManager.getSectionProvider(section);
 		return section.createHandler(provider);
 	}
