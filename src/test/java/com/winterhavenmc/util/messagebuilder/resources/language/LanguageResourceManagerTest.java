43f869a3acdf843f1a942e6d6741f7a460f08e1e
Tim Savage <tim.savage@gmail.com>
2025-04-27 19:30:22 -0500

    rename test class

--- src/test/java/com/winterhavenmc/util/messagebuilder/resources/language/LanguageResourceManagerTest.java
+++ src/test/java/com/winterhavenmc/util/messagebuilder/resources/language/LanguageResourceManagerTest.java
@@ -43,7 +43,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(MockitoExtension.class)
-class SectionResourceManagerTest
+class LanguageResourceManagerTest
 {
 	@Mock
 	LanguageResourceInstaller languageResourceInstallerMock;
