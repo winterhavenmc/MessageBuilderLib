a83486d65efca0a729a3808f7b9247e9eab1254b
Tim Savage <tim.savage@gmail.com>
2025-05-03 02:21:50 -0500

    refactor to extract strings

--- src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/extractor/FieldExtractor.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/extractor/FieldExtractor.java
@@ -29,65 +29,62 @@ import com.winterhavenmc.library.messagebuilder.pipeline.adapters.quantity.Quant
 import com.winterhavenmc.library.messagebuilder.pipeline.adapters.uuid.Identifiable;
 import com.winterhavenmc.library.messagebuilder.pipeline.adapters.uuid.UniqueIdAdapter;
 import com.winterhavenmc.library.messagebuilder.keys.MacroKey;
-import com.winterhavenmc.library.messagebuilder.pipeline.containers.MacroObjectMap;
+import com.winterhavenmc.library.messagebuilder.pipeline.containers.MacroStringMap;
 import org.bukkit.Location;
-
 
 import static com.winterhavenmc.library.messagebuilder.pipeline.adapters.Adapter.BuiltIn.*;
 
 
 public class FieldExtractor implements Extractor
 {
-	@Override
-	public <T> MacroObjectMap extract(Adapter adapter, T adapted, MacroKey baseKey)
+	public <T> MacroStringMap extract(Adapter adapter, T adapted, MacroKey baseKey)
 	{
-		MacroObjectMap fields = new MacroObjectMap();
+		MacroStringMap fields = new MacroStringMap();
 
 		switch (adapter)
 		{
 			case NameAdapter ignored when adapted instanceof Nameable nameable ->
 			{
+				fields.put(baseKey, nameable.getName());
 				baseKey.append(NAME).ifPresent(macroKey -> fields.put(macroKey, nameable.getName()));
-				fields.putIfAbsent(baseKey, nameable.getName());
 			}
 
 			case DisplayNameAdapter ignored when adapted instanceof DisplayNameable displayNameable ->
 			{
+				fields.put(baseKey, displayNameable.getDisplayName());
 				baseKey.append(DISPLAY_NAME).ifPresent(macroKey -> fields.put(macroKey, displayNameable.getDisplayName()));
-				fields.putIfAbsent(baseKey, displayNameable.getDisplayName());
 			}
 
 			case UniqueIdAdapter ignored when adapted instanceof Identifiable identifiable ->
 			{
-				baseKey.append(UUID).ifPresent(macroKey -> fields.put(macroKey, identifiable.getUniqueId()));
-				fields.putIfAbsent(baseKey, identifiable.getUniqueId());
+				fields.put(baseKey, identifiable.getUniqueId().toString());
+				baseKey.append(UUID).ifPresent(macroKey -> fields.put(macroKey, identifiable.getUniqueId().toString()));
 			}
 
 			case LocationAdapter ignored when adapted instanceof Locatable locatable ->
 			{
-				if (!baseKey.toString().endsWith(LOCATION.name()))
+				Location location = locatable.getLocation();
+				if (location != null)
 				{
-					baseKey = baseKey.append(LOCATION).orElse(baseKey);
+					baseKey.append(LOCATION).ifPresent(locationKey ->
+					{
+						fields.put(locationKey, getLocationString(locatable.getLocation()));
+						locationKey.append(LocationField.STRING).ifPresent(macroKey -> fields.put(macroKey, getLocationString(location)));
+						locationKey.append(LocationField.WORLD).ifPresent(macroKey -> fields.put(macroKey, getLocationWorldName(location)));
+						locationKey.append(LocationField.X).ifPresent(macroKey -> fields.put(macroKey, String.valueOf(location.getBlockX())));
+						locationKey.append(LocationField.Y).ifPresent(macroKey -> fields.put(macroKey, String.valueOf(location.getBlockY())));
+						locationKey.append(LocationField.Z).ifPresent(macroKey -> fields.put(macroKey, String.valueOf(location.getBlockZ())));
+					});
 				}
-
-				fields.putIfAbsent(baseKey, getLocationString(locatable.getLocation()));
-				baseKey.append(LocationField.STRING).ifPresent(macroKey ->
-						fields.putIfAbsent(macroKey, getLocationString(locatable.getLocation())));
-				baseKey.append(LocationField.WORLD).ifPresent(macroKey ->
-						fields.putIfAbsent(macroKey, getLocationWorldName(locatable.getLocation())));
-				baseKey.append(LocationField.X).ifPresent(macroKey ->
-						fields.putIfAbsent(macroKey, locatable.getLocation().getBlockX()));
-				baseKey.append(LocationField.Y).ifPresent(macroKey ->
-						fields.putIfAbsent(macroKey, locatable.getLocation().getBlockY()));
-				baseKey.append(LocationField.Z).ifPresent(macroKey ->
-						fields.putIfAbsent(macroKey, locatable.getLocation().getBlockZ()));
 			}
 
 			case QuantityAdapter ignored when adapted instanceof Quantifiable quantifiable ->
 			{
-				baseKey.append(QUANTITY).ifPresent(macroKey -> fields.put(macroKey, quantifiable.getQuantity()));
-				fields.putIfAbsent(baseKey, quantifiable.getQuantity());
+				String quantityString = String.valueOf(quantifiable.getQuantity());
+				fields.put(baseKey, quantityString);
+				baseKey.append(QUANTITY).ifPresent(macroKey -> fields.put(macroKey, quantityString));
 			}
+
 			default -> {} // no-op
 		}
 
