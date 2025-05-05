855027f8a767e3ade1977275574df919bbb77748
Tim Savage <tim.savage@gmail.com>
2025-05-05 14:50:32 -0500

    refactor

--- src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/MessagePipeline.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/MessagePipeline.java
@@ -20,7 +20,7 @@ package com.winterhavenmc.library.messagebuilder.pipeline;
 import com.winterhavenmc.library.messagebuilder.model.message.ValidMessage;
 import com.winterhavenmc.library.messagebuilder.pipeline.cooldown.CooldownKey;
 import com.winterhavenmc.library.messagebuilder.pipeline.cooldown.CooldownMap;
-import com.winterhavenmc.library.messagebuilder.pipeline.replacer.MacroReplacer;
+import com.winterhavenmc.library.messagebuilder.pipeline.processor.MessageProcessor;
 import com.winterhavenmc.library.messagebuilder.pipeline.retriever.MessageRetriever;
 import com.winterhavenmc.library.messagebuilder.pipeline.sender.Sender;
 import com.winterhavenmc.library.messagebuilder.model.language.FinalMessageRecord;
@@ -35,43 +35,47 @@ import java.util.function.Function;
 public final class MessagePipeline implements Pipeline
 {
 	private final MessageRetriever messageRetriever;
-	private final MacroReplacer macroReplacer;
+	private final MessageProcessor messageProcessor;
 	private final CooldownMap cooldownMap;
 	private final List<Sender> senders;
 
 
 	public MessagePipeline(final MessageRetriever messageRetriever,
-						   final MacroReplacer macroReplacer,
+						   final MessageProcessor messageProcessor,
 						   final CooldownMap cooldownMap,
 						   final List<Sender> senders)
 	{
 		this.messageRetriever = messageRetriever;
-		this.macroReplacer = macroReplacer;
+		this.messageProcessor = messageProcessor;
 		this.cooldownMap = cooldownMap;
 		this.senders = senders;
 	}
 
 
 	@Override
-	public void process(final ValidMessage message)
+	public void initiate(final ValidMessage message)
 	{
-		Function<CooldownKey, Optional<ValidMessageRecord>> retrieveMessage = key ->
-				(messageRetriever.getRecord(message.getMessageKey()) instanceof ValidMessageRecord valid)
-						? Optional.of(valid)
+		// queries CooldownMap, returns ValidMessageRecord
+		Function<CooldownKey, Optional<ValidMessageRecord>> retrieveMessageRecord = key ->
+				(messageRetriever.getRecord(message.getMessageKey()) instanceof ValidMessageRecord validMessageRecord)
+						? Optional.of(validMessageRecord)
 						: Optional.empty();
 
-		Function<ValidMessageRecord, FinalMessageRecord> resolveMacros = messageRecord -> macroReplacer
-				.replaceMacros(messageRecord, message.getObjectMap());
+		// transforms ValidMessageRecord into FinalMessageRecord
+		Function<ValidMessageRecord, FinalMessageRecord> processMessageRecord = messageRecord -> messageProcessor
+				.process(messageRecord, message.getObjectMap());
 
-		Consumer<FinalMessageRecord> sendMessage = processed -> senders
+		// consumes FinalMessageRecord
+		Consumer<FinalMessageRecord> sendMessageRecord = processed -> senders
 				.forEach(sender -> sender.send(message.getRecipient(), processed));
 
 
-		CooldownKey.of(message.getRecipient(), message.getMessageKey())
+		// process message through pipeline
+		CooldownKey.of(message)
 				.filter(cooldownMap::notCooling)
-				.flatMap(retrieveMessage)
-				.map(resolveMacros)
-				.ifPresent(sendMessage);
+				.flatMap(retrieveMessageRecord)
+				.map(processMessageRecord)
+				.ifPresent(sendMessageRecord);
 	}
 
 }
