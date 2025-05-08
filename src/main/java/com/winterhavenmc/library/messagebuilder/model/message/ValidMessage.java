298de8a19b4466615cac2a0f0ceede688429d919
Tim Savage <tim.savage@gmail.com>
2025-05-08 15:52:59 -0500

    remove unused import

--- src/main/java/com/winterhavenmc/library/messagebuilder/model/message/ValidMessage.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/model/message/ValidMessage.java
@@ -28,7 +28,6 @@ import com.winterhavenmc.library.messagebuilder.validation.ValidationException;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.Map;
 import java.util.Objects;
 
 import static com.winterhavenmc.library.messagebuilder.validation.ErrorMessageKey.PARAMETER_INVALID;
