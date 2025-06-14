a5f96100bebcf89f68fdf774cea2182a62e485c3
Tim Savage <tim.savage@gmail.com>
2025-06-14 18:27:31 -0500

    initial commit

---
+++ src/test/java/com/winterhavenmc/library/messagebuilder/pipeline/adapters/killer/KillerAdapterTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2025 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.library.messagebuilder.pipeline.adapters.killer;
+
+import com.winterhavenmc.library.messagebuilder.pipeline.adapters.AdapterContextContainer;
+import com.winterhavenmc.library.messagebuilder.pipeline.resolvers.worldname.WorldNameResolver;
+
+import org.bukkit.entity.AnimalTamer;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class KillerAdapterTest
+{
+	@Mock WorldNameResolver worldNameResolverMock;
+	@Mock AdapterContextContainer adapterContextContainerMock;
+	@Mock Player playerMock;
+
+	class TestObject implements Killable
+	{
+		@Override
+		public AnimalTamer getKiller()
+		{
+			return playerMock;
+		}
+	}
+
+
+	@Test @DisplayName("adapt with valid Player")
+	public void adapt_with_valid_player_returns_adapted_player()
+	{
+		// Arrange
+		TestObject testObject = new TestObject();
+
+		// Act
+		Optional<Killable> adapter = new KillerAdapter().adapt(testObject);
+		Optional<AnimalTamer> killer = adapter.map(Killable::getKiller);
+
+		// Assert
+		assertTrue(killer.isPresent());
+		assertEquals(playerMock, killer.get(), "The adapter should return the mock Player.");
+	}
+
+
+	@Test @DisplayName("adapt with null")
+	public void adapt_with_null_parameter_returns_empty_optional()
+	{
+		// Arrange & Act
+		Optional<Killable> adapter = new KillerAdapter().adapt(null);
+
+		// Assert
+		assertEquals(Optional.empty(), adapter, "The adapter should return an empty optional for a null parameter.");
+	}
+}
