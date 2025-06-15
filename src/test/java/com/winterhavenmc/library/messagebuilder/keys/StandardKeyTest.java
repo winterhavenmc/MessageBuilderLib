e972cb40cf93c9431e08fd577ddb3a1bad599444
Tim Savage <tim.savage@gmail.com>
2025-06-15 14:53:57 -0500

    rename test methods

--- src/test/java/com/winterhavenmc/library/messagebuilder/keys/StandardKeyTest.java
+++ src/test/java/com/winterhavenmc/library/messagebuilder/keys/StandardKeyTest.java
@@ -28,14 +28,14 @@ class StandardKeyTest
 	class EqualsTests
 	{
 		@Test
-		void testEquals_same_instance()
+		void key_equals_same_instance()
 		{
 			MacroKey key = MacroKey.of("FOO").orElseThrow();
 			assertEquals(key, key);
 		}
 
 		@Test
-		void testEquals_same_type_and_values_macro()
+		void key_equals_same_type_and_values_macro()
 		{
 			MacroKey key1 = MacroKey.of("ABC").orElseThrow();
 			MacroKey key2 = MacroKey.of("ABC").orElseThrow();
@@ -43,7 +43,7 @@ class StandardKeyTest
 		}
 
 		@Test
-		void testEquals_same_types_and_values_record()
+		void key_equals_same_types_and_values_record()
 		{
 			RecordKey key1 = RecordKey.of("XYZ").orElseThrow();
 			RecordKey key2 = RecordKey.of("XYZ").orElseThrow();
@@ -51,7 +51,7 @@ class StandardKeyTest
 		}
 
 		@Test
-		void testEquals_same_types_different_values_macro()
+		void key_not_equals_same_types_different_values_macro()
 		{
 			MacroKey key1 = MacroKey.of("ABC").orElseThrow();
 			MacroKey key2 = MacroKey.of("XYZ").orElseThrow();
@@ -59,7 +59,7 @@ class StandardKeyTest
 		}
 
 		@Test
-		void testEquals_same_types_different_values_record()
+		void key_not_equals_same_types_different_values_record()
 		{
 			RecordKey key1 = RecordKey.of("ABC").orElseThrow();
 			RecordKey key2 = RecordKey.of("XYZ").orElseThrow();
@@ -67,7 +67,7 @@ class StandardKeyTest
 		}
 
 		@Test
-		void testEquals_different_types_same_values()
+		void key_not_equals_different_types_same_values()
 		{
 			MacroKey key1 = MacroKey.of("ABC").orElseThrow();
 			RecordKey key2 = RecordKey.of("ABC").orElseThrow();
@@ -75,7 +75,7 @@ class StandardKeyTest
 		}
 
 		@Test
-		void testEquals_different_types_different_values_macro()
+		void key_not_equals_different_types_different_values_macro()
 		{
 			MacroKey key1 = MacroKey.of("ABC").orElseThrow();
 			RecordKey key2 = RecordKey.of("XYZ").orElseThrow();
@@ -84,21 +84,21 @@ class StandardKeyTest
 
 
 		@Test
-		void testEquals_null_macro()
+		void key_not_equals_null_macro()
 		{
 			MacroKey key1 = MacroKey.of("ABC").orElseThrow();
 			assertNotEquals(null, key1);
 		}
 
 		@Test
-		void testEquals_null_record()
+		void key_not_equals_null_record()
 		{
 			RecordKey key1 = RecordKey.of("ABC").orElseThrow();
 			assertNotEquals(null, key1);
 		}
 
 		@Test
-		void testEquals_different_class_macro()
+		void key_not_equals_different_class_macro()
 		{
 			MacroKey key1 = MacroKey.of("ABC").orElseThrow();
 			String key2 = "ABC";
@@ -106,7 +106,7 @@ class StandardKeyTest
 		}
 
 		@Test
-		void testEquals_different_class_record()
+		void key_not_equals_different_class_record()
 		{
 			RecordKey key1 = RecordKey.of("ABC").orElseThrow();
 			String key2 = "ABC";
@@ -116,27 +116,27 @@ class StandardKeyTest
 
 
 	@Test
-	void testHashCode_equalMacroKeys() {
+	void key_hashCode_equal_MacroKeys() {
 		MacroKey a = MacroKey.of("MATCH").orElseThrow();
 		MacroKey b = MacroKey.of("MATCH").orElseThrow();
 		assertEquals(a.hashCode(), b.hashCode());
 	}
 
 	@Test
-	void testHashCode_equalRecordKeys() {
+	void key_hashCode_equal_RecordKeys() {
 		RecordKey a = RecordKey.of("MATCH").orElseThrow();
 		RecordKey b = RecordKey.of("MATCH").orElseThrow();
 		assertEquals(a.hashCode(), b.hashCode());
 	}
 
 	@Test
-	void testToString_returnsWrappedString_macro() {
+	void ToString_returns_wrapped_string_macro() {
 		MacroKey key = MacroKey.of("FOOBAR").orElseThrow();
 		assertEquals("FOOBAR", key.toString());
 	}
 
 	@Test
-	void testToString_returnsWrappedString_record() {
+	void toString_returns_wrapped_string_record() {
 		RecordKey key = RecordKey.of("LOREM").orElseThrow();
 		assertEquals("LOREM", key.toString());
 	}
