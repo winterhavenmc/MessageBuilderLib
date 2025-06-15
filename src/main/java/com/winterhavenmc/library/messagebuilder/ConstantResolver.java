9169365dc91ab497db06fd0e7577b3b484c3428b
Tim Savage <tim.savage@gmail.com>
2025-06-15 13:25:40 -0500

    add javadocs

--- src/main/java/com/winterhavenmc/library/messagebuilder/ConstantResolver.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/ConstantResolver.java
@@ -32,43 +32,78 @@ public class ConstantResolver
 	private final QueryHandler<ConstantRecord> constantQueryHandler;
 
 
+	/**
+	 * Class constructor
+	 *
+	 * @param queryHandlerFactory an instance of the query handler factory
+	 */
 	ConstantResolver(final QueryHandlerFactory queryHandlerFactory)
 	{
 		this.constantQueryHandler = queryHandlerFactory.getQueryHandler(Section.CONSTANTS);
 	}
 
 
+	/**
+	 * Retrieves a constant record using the constant query handler
+	 *
+	 * @param key a string to be used as the key for the record to be retrieved
+	 * @return an Optional constant record, or an empty Optional if no record could be retrieved
+	 */
 	private Optional<ConstantRecord> getConstantRecord(final RecordKey key)
 	{
 		return Optional.ofNullable(constantQueryHandler.getRecord(key));
 	}
 
 
+	/**
+	 * Retrieves an Optional String from the constants section of the language file
+	 *
+	 * @param key a string to be used as the key for the String to be retrieved
+	 * @return an Optional String, or an empty Optional if no String record could be retrieved
+	 */
 	public Optional<String> getString(final String key)
 	{
 		return RecordKey.of(key)
 				.flatMap(this::getConstantRecord)
-				.flatMap(this::fetchStringValue);
+				.flatMap(this::extractStringValue);
 	}
 
 
+	/**
+	 * Retrieves an Optional Integer from the constants section of the language file
+	 *
+	 * @param key a string to be used as the key for the Integer to be retrieved
+	 * @return an Optional Integer, or an empty Optional if no Integer record could be retrieved
+	 */
 	public Optional<Integer> getInteger(final String key)
 	{
 		return RecordKey.of(key)
 				.flatMap(this::getConstantRecord)
-				.flatMap(this::fetchIntegerValue);
+				.flatMap(this::extractIntegerValue);
 	}
 
 
+	/**
+	 * Retrieves an Optional Boolean from the constants section of the language file
+	 *
+	 * @param key a string to be used as the key for the Boolean to be retrieved
+	 * @return an Optional Boolean, or an empty Optional if no Boolean record could be retrieved
+	 */
 	public Optional<Boolean> getBoolean(final String key)
 	{
 		return RecordKey.of(key)
 				.flatMap(this::getConstantRecord)
-				.flatMap(this::fetchBooleanValue);
+				.flatMap(this::extractBooleanValue);
 	}
 
 
-	Optional<String> fetchStringValue(final ConstantRecord record)
+	/**
+	 * Private helper method to extract a String value from a ConstantRecord
+	 *
+	 * @param record the ConstantRecord to extract a value
+	 * @return an Optional String, or an empty Optional if a String value could not be extracted from the ConstantRecord
+	 */
+	private Optional<String> extractStringValue(final ConstantRecord record)
 	{
 		return (record instanceof ValidConstantRecord validRecord && validRecord.value() instanceof String string)
 				? Optional.of(string)
@@ -76,7 +111,13 @@ public class ConstantResolver
 	}
 
 
-	Optional<Integer> fetchIntegerValue(final ConstantRecord record)
+	/**
+	 * Private helper method to extract an Integer value from a ConstantRecord
+	 *
+	 * @param record the ConstantRecord to extract a value
+	 * @return an Optional Integer, or an empty Optional if an Integer value could not be extracted from the ConstantRecord
+	 */
+	private Optional<Integer> extractIntegerValue(final ConstantRecord record)
 	{
 		return (record instanceof ValidConstantRecord validRecord && validRecord.value() instanceof Integer integer)
 				? Optional.of(integer)
@@ -84,7 +125,13 @@ public class ConstantResolver
 	}
 
 
-	Optional<Boolean> fetchBooleanValue(final ConstantRecord record)
+	/**
+	 * Private helper method to extract a Boolean value from a ConstantRecord
+	 *
+	 * @param record the ConstantRecord to extract a value
+	 * @return an Optional Boolean, or an empty Optional if a Boolean value could not be extracted from the ConstantRecord
+	 */
+	private Optional<Boolean> extractBooleanValue(final ConstantRecord record)
 	{
 		return (record instanceof ValidConstantRecord validRecord && validRecord.value() instanceof Boolean bool)
 				? Optional.of(bool)
