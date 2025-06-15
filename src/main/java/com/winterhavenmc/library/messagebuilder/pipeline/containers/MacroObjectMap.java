07fd5b44523c88a3a7d29ffd4e4d6ac2b99201f8
Tim Savage <tim.savage@gmail.com>
2025-06-15 07:30:47 -0500

    add javadocs

--- src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/containers/MacroObjectMap.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/containers/MacroObjectMap.java
@@ -99,12 +99,22 @@ public class MacroObjectMap
 	}
 
 
+	/**
+	 * Return the number of entries in the map as int
+	 *
+	 * @return {@code int} number of entries in the map
+	 */
 	public int size()
 	{
 		return INTERNAL_MAP.size();
 	}
 
 
+	/**
+	 * Return {@code boolean} representing whether the map contains any entries
+	 *
+	 * @return {@code true} if the map contains no entries, or {@code false} if it contains one or more entries
+	 */
 	public boolean isEmpty()
 	{
 		return INTERNAL_MAP.isEmpty();
