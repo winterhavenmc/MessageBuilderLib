9319a4e8d7259b76ba41fbac0f6d4971dac71bf0
Tim Savage <tim.savage@gmail.com>
2025-06-18 08:56:39 -0500

    edit javadocs

--- src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/adapters/looter/package-info.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/pipeline/adapters/looter/package-info.java
@@ -6,7 +6,7 @@
  * permission to retrieve contents or rewards from a container, chest, or drop.
  *
  * <p>Importantly, this looter concept is not tied to an actual looting event. Instead, it expresses
- * ownership or entitlement prior to looting. This allows for message macros like {@code %OBJECT.LOOTER%}
+ * ownership or entitlement prior to looting. This allows for message macros like {@code {OBJECT.LOOTER}}
  * to inform players of access restrictions or intended claimants.
  *
  * <p>The {@link com.winterhavenmc.library.messagebuilder.pipeline.adapters.looter.LooterAdapter}
