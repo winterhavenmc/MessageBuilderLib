f98b3a3376fc5189b774baa87b73c17e3553cb85
Tim Savage <tim.savage@gmail.com>
2025-06-20 04:18:56 -0500

    add tests

--- src/test/java/com/winterhavenmc/library/messagebuilder/validation/ValidatorTest.java
+++ src/test/java/com/winterhavenmc/library/messagebuilder/validation/ValidatorTest.java
@@ -17,35 +17,58 @@
 
 package com.winterhavenmc.library.messagebuilder.validation;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Objects;
+
+import static com.winterhavenmc.library.messagebuilder.validation.ErrorMessageKey.PARAMETER_NULL;
+import static com.winterhavenmc.library.messagebuilder.validation.ErrorMessageKey.STRING_BLANK;
+import static com.winterhavenmc.library.messagebuilder.validation.Parameter.ADAPTER;
+import static com.winterhavenmc.library.messagebuilder.validation.Parameter.MACRO;
+import static com.winterhavenmc.library.messagebuilder.validation.Validator.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class ValidatorTest
 {
-	@Test
-	void testFormatMessage()
+	@Test @DisplayName("formatMessage() retrieves message from resource bundle.")
+	void formatMessage_retrieves_message_from_resource_bundle()
 	{
-		String result = Validator.formatMessage(ErrorMessageKey.PARAMETER_NULL, Parameter.RECIPIENT);
+		// Act
+		String result = Validator.formatMessage(PARAMETER_NULL, Parameter.RECIPIENT);
 
+		// Assert
 		assertEquals("The parameter 'recipient' cannot be null.", result);
 	}
 
 
-//	@Test
-//	void testGetConfiguredLocale()
-//	{
-//		Locale result = getConfiguredLocale();
-//
-//		assertEquals(Locale.getDefault(), result);
-//	}
-
-
 	@Test
-	void testBundleName()
+	void bundleName_matches_resource_bundle()
 	{
 		assertEquals("exception.messages", Validator.BUNDLE_NAME);
 	}
 
+
+	@Test
+	void throwing_throws_validation_exception()
+	{
+		assertThrows(ValidationException.class, () ->
+				validate(null, Objects::isNull, throwing(PARAMETER_NULL, ADAPTER)));
+	}
+
+
+	@Test
+	void throwing_does_not_throws_with_valid_parameter()
+	{
+		assertDoesNotThrow(() ->
+				validate("not null", Objects::isNull, throwing(PARAMETER_NULL, MACRO)));
+	}
+
+
+	@Test
+	void logging_logs_validation_error()
+	{
+		assertDoesNotThrow(() -> validate("Valid String", String::isBlank, logging(LogLevel.WARN, STRING_BLANK, MACRO)));
+	}
 }
