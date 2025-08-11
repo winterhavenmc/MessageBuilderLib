156f021ccdcc2370ca82a88a7dae3306c931e253
Tim Savage <tim.savage@gmail.com>
2025-08-11 05:51:55 -0500

    edit for style

--- src/main/java/com/winterhavenmc/library/messagebuilder/macro/processor/AbstractProcessor.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/macro/processor/AbstractProcessor.java
@@ -21,28 +21,30 @@ import com.winterhavenmc.library.messagebuilder.LanguageHandler;
 import com.winterhavenmc.library.messagebuilder.macro.MacroObjectMap;
 
 
-public abstract class AbstractProcessor {
-
+public abstract class AbstractProcessor
+{
 	protected final LanguageHandler languageHandler;
 	protected final static String UNKNOWN_VALUE = "???";
 
 
-	AbstractProcessor(final LanguageHandler languageHandler) {
+	AbstractProcessor(final LanguageHandler languageHandler)
+	{
 		this.languageHandler = languageHandler;
 	}
 
 	public abstract ResultMap execute(final MacroObjectMap macroObjectMap, final String key, final Object object);
 
 
-	protected ResultMap mapName(final String key, final String name) {
-
+	protected ResultMap mapName(final String key, final String name)
+	{
 		ResultMap resultMap = new ResultMap();
 
 		//TODO: This creates a key with the original key string, and additionally a second key suffixed with '_NAME' if that is not already the case.
 		// This was done for backward compatibility purposes, and will be removed or modified at some point in the future
 
 		String keyExtra = "_NAME";
-		if (key.endsWith("_NAME")) {
+		if (key.endsWith("_NAME"))
+		{
 			keyExtra = "";
 		}
 
@@ -53,30 +55,33 @@ public abstract class AbstractProcessor {
 	}
 
 
-	ResultMap mapConfigItemName(final MacroObjectMap macroObjectMap, final String... keys) {
-
+	ResultMap mapConfigItemName(final MacroObjectMap macroObjectMap, final String... keys)
+	{
 		ResultMap resultMap = new ResultMap();
 
 		// put item name in result map for each passed key
-		for (String key : keys) {
+		for (String key : keys)
+		{
 			resultMap.put(key, getConfigItemName(macroObjectMap));
 		}
 		return resultMap;
 	}
 
 
-	private String getConfigItemName(final MacroObjectMap macroObjectMap) {
-
+	private String getConfigItemName(final MacroObjectMap macroObjectMap)
+	{
 		int itemQuantity = 1;
 
 		// if macro ITEM_QUANTITY exists and is integer, set quantity
-		if (macroObjectMap.containsKey("ITEM_QUANTITY") && macroObjectMap.get("ITEM_QUANTITY") instanceof Integer integer) {
+		if (macroObjectMap.containsKey("ITEM_QUANTITY") && macroObjectMap.get("ITEM_QUANTITY") instanceof Integer integer)
+		{
 			itemQuantity = integer;
 		}
 
 		// get singular or plural item name from message file
 		//noinspection SwitchStatementWithTooFewBranches
-		return switch (itemQuantity) {
+		return switch (itemQuantity)
+		{
 			case 1 -> languageHandler.getItemName().orElse(UNKNOWN_VALUE);
 			default -> languageHandler.getItemNamePlural().orElse(UNKNOWN_VALUE);
 		};
