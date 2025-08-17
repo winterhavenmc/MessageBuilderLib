64871f4414bf82c70f936922ffbbf71546e982fe
Tim Savage <tim.savage@gmail.com>
2025-08-17 05:57:33 -0500

    edit comment

--- src/main/java/com/winterhavenmc/library/messagebuilder/keys/StandardKey.java
+++ src/main/java/com/winterhavenmc/library/messagebuilder/keys/StandardKey.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
  */
 public sealed interface StandardKey permits MacroKey, RecordKey
 {
-	// valid string must begin with uppercase alpha only and may contain alpha, digits, underscore or period
+	// valid key string must begin with uppercase alpha and then may contain upper/lower alpha, digits, underscores or periods
 	Pattern VALID_KEY_PATTERN = Pattern.compile("^[A-Z][a-zA-Z\\d_.]*$");
 	Predicate<String> IS_INVALID_KEY = string -> !VALID_KEY_PATTERN.matcher(string).matches();
 }
