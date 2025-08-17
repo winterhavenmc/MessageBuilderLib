44eb4578e2e32857336b0a3600365d8d7ea31f13
Tim Savage <tim.savage@gmail.com>
2025-08-17 03:00:07 -0500

    initial commit

---
+++ src/main/java/com/winterhavenmc/library/messagebuilder/model/language/InvalidRecordReason.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2025 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.library.messagebuilder.model.language;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+
+public enum InvalidRecordReason
+{
+	CONSTANT_ENTRY_MISSING("Missing constant entry."),
+	CONSTANT_KEY_INVALID("Invalid constant key."),
+
+	ITEM_ENTRY_MISSING("Missing item entry."),
+	ITEM_KEY_INVALID("Invalid item key."),
+
+	MESSAGE_ENTRY_MISSING("Missing message entry."),
+	MESSAGE_KEY_INVALID("Invalid message key."),
+	;
+
+	private final String defaultMessage;
+
+
+	InvalidRecordReason(final String defaultMessage)
+	{
+		this.defaultMessage = defaultMessage;
+	}
+
+
+	public String getDefaultMessage()
+	{
+		return this.defaultMessage;
+	}
+
+
+	public String getLocalizeMessage(final Locale locale)
+	{
+		try
+		{
+			final ResourceBundle bundle = ResourceBundle.getBundle("messages", locale);
+			return bundle.getString(name());
+		}
+		catch (MissingResourceException exception)
+		{
+			return this.defaultMessage;
+		}
+	}
+
+
+	public String getLocalizeMessage(final Locale locale, final Object... objects)
+	{
+		try
+		{
+			final ResourceBundle bundle = ResourceBundle.getBundle("messages", locale);
+			String pattern = bundle.getString(name());
+			return MessageFormat.format(pattern, objects);
+		}
+		catch (MissingResourceException exception)
+		{
+			return this.defaultMessage;
+		}
+	}
+
+}
