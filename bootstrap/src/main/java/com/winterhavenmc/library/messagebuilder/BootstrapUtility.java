1a313fb234f745c92273664ba4b0ccdad13a8b12
Tim Savage <tim.savage@gmail.com>
2025-10-23 18:26:28 -0500

    use static factory method to create EnabledWorldsProvider

--- bootstrap/src/main/java/com/winterhavenmc/library/messagebuilder/BootstrapUtility.java
+++ bootstrap/src/main/java/com/winterhavenmc/library/messagebuilder/BootstrapUtility.java
@@ -218,7 +218,7 @@ public final class BootstrapUtility
 
 	static EnabledWorldsProvider createEnabledWorldsProvider(final Plugin plugin)
 	{
-		return new BukkitEnabledWorldsProvider(plugin);
+		return BukkitEnabledWorldsProvider.create(plugin);
 	}
 
 }
