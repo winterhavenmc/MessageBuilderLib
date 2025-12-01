c50a4b18cdaf8cac550d578462e0fa644754a16e
Tim Savage <tim.savage@gmail.com>
2025-12-01 00:18:50 -0600

    add javadoc

--- models/src/main/java/com/winterhavenmc/library/messagebuilder/models/configuration/worlds/WorldRepository.java
+++ models/src/main/java/com/winterhavenmc/library/messagebuilder/models/configuration/worlds/WorldRepository.java
@@ -8,6 +8,37 @@ import java.util.Optional;
 import java.util.UUID;
 
 
+/**
+ * <h1>World Repository</h1>
+ * <p>
+ * The world repository provides utility methods for querying world
+ * parameters and configuration settings. Its primary purpose is to
+ * provide an internal list of enabled worlds for the plugin to reference,
+ * which is the set difference between the "enabled-worlds" string list
+ * setting and the "disabled-worlds" string list setting in the
+ * plugin's config.yml file.
+ * <p>
+ * Other convenience methods are provided for querying world parameters that
+ * may return a different result than the standard Bukkit methods. For instance,
+ * if the Multiverse plugin is available, it will be used to query the world
+ * alias if set, or the spawn location which may differ from the world's
+ * spawn.
+ * <p>
+ * Note that Multiverse spawn locations include yaw and pitch, and
+ * therefore allow more precise positioning of a player on respawn.
+ * <p>
+ * The world repository and its methods are always accessed via an instance
+ * of MessageBuilder.
+ * <p>
+ * <i>Examples:</i>
+ {@snippet :
+   boolean enabled = messageBuilder.worlds().isEnabled(worldUid);
+
+   String worldName = messageBuilder.worlds().aliasOrName(worldUid);
+
+   Location spawnLocation = messageBuilder.worlds().spawnLocation(worldUid);
+ }
+ */
 public interface WorldRepository extends ConfigProvider<EnabledWorldsSetting>
 {
 	/**
