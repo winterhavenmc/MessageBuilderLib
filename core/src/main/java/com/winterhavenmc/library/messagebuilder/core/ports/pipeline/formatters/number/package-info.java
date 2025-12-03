ffde6bf2e4d8d9cee8207563e198682c3e91dc83
Tim Savage <tim.savage@gmail.com>
2025-12-02 20:06:47 -0600

    edit javadoc

--- core/src/main/java/com/winterhavenmc/library/messagebuilder/core/ports/pipeline/formatters/number/package-info.java
+++ core/src/main/java/com/winterhavenmc/library/messagebuilder/core/ports/pipeline/formatters/number/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Provides an interface that declares a method for formatting a number
+ * Provides an interface for locale-aware number formatting for use in the message-building pipeline.
  */
 package com.winterhavenmc.library.messagebuilder.core.ports.pipeline.formatters.number;
