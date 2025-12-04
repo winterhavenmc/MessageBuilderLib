c520e47563ccaf2f4afe15d362db01881bc5b7d4
Tim Savage <tim.savage@gmail.com>
2025-12-04 14:22:58 -0600

    initial commit

---
+++ core/src/main/java/com/winterhavenmc/library/messagebuilder/core/ports/pipeline/accessors/pluralname/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides an interface to allow extraction of a plural name field from implementing classes.
+ */
+package com.winterhavenmc.library.messagebuilder.core.ports.pipeline.accessors.pluralname;
