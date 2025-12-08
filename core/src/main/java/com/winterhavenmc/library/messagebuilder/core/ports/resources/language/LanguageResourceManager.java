dacee3451999a58f15da863957f4fe4dbeed6bda
Tim Savage <tim.savage@gmail.com>
2025-12-08 02:41:33 -0600

    initial commit

---
+++ core/src/main/java/com/winterhavenmc/library/messagebuilder/core/ports/resources/language/LanguageResourceManager.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022-2025 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.library.messagebuilder.core.ports.resources.language;
+
+import com.winterhavenmc.library.messagebuilder.core.ports.resources.ResourceManager;
+import com.winterhavenmc.library.messagebuilder.core.ports.resources.SectionProvider;
+import com.winterhavenmc.library.messagebuilder.models.language.Section;
+
+
+/**
+ * An interface that describes a language resource manager that is responsible for installing resources
+ * from the plugin jar file to the plugin data directory, as well as loading and reloading
+ * the installed files at startup or issuance of the {@code reload} command.
+ */
+public interface LanguageResourceManager extends ResourceManager
+{
+	SectionProvider getSectionProvider(final Section section);
+}
