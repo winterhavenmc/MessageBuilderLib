1da59c0ab2fc0f9a997ba8d50ec87deb3ae38f2d
Tim Savage <tim.savage@gmail.com>
2025-12-15 05:44:10 -0600

    rename/relocate interface

--- adapters/src/main/java/com/winterhavenmc/library/messagebuilder/adapters/pipeline/retrievers/itemname/ItemDisplayNameRetriever.java
+++ adapters/src/main/java/com/winterhavenmc/library/messagebuilder/adapters/pipeline/retrievers/itemname/ItemDisplayNameRetriever.java
@@ -1,6 +1,6 @@
 package com.winterhavenmc.library.messagebuilder.adapters.pipeline.retrievers.itemname;
 
-import com.winterhavenmc.library.messagebuilder.adapters.pipeline.retrievers.Retriever;
+import com.winterhavenmc.library.messagebuilder.core.ports.pipeline.retrievers.ItemStackNameRetriever;
 import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -8,7 +8,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.Optional;
 
 
-public class ItemDisplayNameRetriever implements Retriever
+public class ItemDisplayNameRetriever implements ItemStackNameRetriever
 {
 	public Optional<String> retrieve(final ItemStack itemStack)
 	{
