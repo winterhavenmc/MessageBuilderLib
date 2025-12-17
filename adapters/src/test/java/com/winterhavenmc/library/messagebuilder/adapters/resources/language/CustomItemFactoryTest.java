1f32738a8b2f6556d2ea41c69788fa84a7705d65
Tim Savage <tim.savage@gmail.com>
2025-12-17 04:46:38 -0600

    remove unused import

--- adapters/src/test/java/com/winterhavenmc/library/messagebuilder/adapters/resources/language/CustomItemFactoryTest.java
+++ adapters/src/test/java/com/winterhavenmc/library/messagebuilder/adapters/resources/language/CustomItemFactoryTest.java
@@ -2,7 +2,6 @@ package com.winterhavenmc.library.messagebuilder.adapters.resources.language;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CustomItemFactoryTest
 {
