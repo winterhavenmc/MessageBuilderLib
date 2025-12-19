a04b9ffdf54fe14c6320759d9595dd585339ce21
Tim Savage <tim.savage@gmail.com>
2025-12-18 22:59:26 -0600

    refine tests

--- models/src/test/java/com/winterhavenmc/library/messagebuilder/models/keys/InvalidKeyReasonTest.java
+++ models/src/test/java/com/winterhavenmc/library/messagebuilder/models/keys/InvalidKeyReasonTest.java
@@ -19,17 +19,38 @@ package com.winterhavenmc.library.messagebuilder.models.keys;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class InvalidKeyReasonTest
 {
-
 	@Test
-	void getDefaultMessage()
+	void getDefaultMessage_returns_valid_defaultMessage()
 	{
 		assertEquals("The parameter 'key' was null.", InvalidKeyReason.KEY_NULL.getDefaultMessage());
 		assertEquals("The parameter 'key' was blank.", InvalidKeyReason.KEY_BLANK.getDefaultMessage());
 		assertEquals("The parameter 'key' was invalid.", InvalidKeyReason.KEY_INVALID.getDefaultMessage());
 	}
+
+	@Test
+	void getLocalizedMessage_returns_valid_bundle_message()
+	{
+		assertEquals("The parameter ''key'' was null.", InvalidKeyReason.KEY_NULL.getLocalizedMessage(Locale.US));
+		assertEquals("The parameter ''key'' was blank.", InvalidKeyReason.KEY_BLANK.getLocalizedMessage(Locale.US));
+		assertEquals("The parameter ''key'' was invalid.", InvalidKeyReason.KEY_INVALID.getLocalizedMessage(Locale.US));
+	}
+
+	@Test
+	void getLocalizedMessage_with_object_parameter_returns_valid_bundle_message()
+	{
+		assertEquals("The parameter 'key' was null.", InvalidKeyReason.KEY_NULL.getLocalizedMessage(Locale.US, "object1", "object2"));
+		assertEquals("The parameter 'key' was blank.", InvalidKeyReason.KEY_BLANK.getLocalizedMessage(Locale.US, "object1", "object2"));
+		assertEquals("The parameter 'key' was invalid.", InvalidKeyReason.KEY_INVALID.getLocalizedMessage(Locale.US, "object1", "object2"));
+	}
 }
+
+// KEY_NULL = The parameter ''key'' was null.
+// KEY_BLANK = The parameter ''key'' was blank.
+// KEY_INVALID = The parameter ''key'' was invalid.
